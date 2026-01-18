254e7481f4a855036768a09ea2c0f5fafcda5d1b
padgettanna <bel.anna90@gmail.com>
2026-01-18 15:45:31 -0800

    Updated UI, added PaymentsPerYear enum class

---
+++ MortgageLoanCalculator.Domain/PaymentsPerYear.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MortgageLoanCalculator.Domain;
+
+public enum PaymentsPerYear : int
+{
+    [Display(Name = "4 (Quarterly)")]
+    Quarterly = 4,
+
+    [Display(Name = "12 (Monthly)")]
+    Monthly = 12,
+
+    [Display(Name = "24 (Bi-weekly)")]
+    Biweekly = 24
+
+}
